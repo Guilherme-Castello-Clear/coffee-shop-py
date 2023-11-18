b38c2612cc0441a90ff8ea9fa298d430b60c4032
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-18 20:36:10 -0300

    Feat: Adding to cart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -15,10 +16,28 @@ class Product(db.Model):
     name = db.Column(db.String(255), unique=True, nullable=False)
     price = db.Column(db.Float, unique=True, nullable=False)
     description = db.Column(db.String(255), unique=True, nullable=False)
+    current_cart = relationship("Cart", back_populates="product")
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product = relationship("Product", back_populates="current_cart")
 
 
 with app.app_context():
     db.create_all()
+
+
+@app.route('/add_to_cart/<int:id>', methods=["GET"])
+def add_to_cart(id):
+    add_product = db.get_or_404(Product, id)
+    new_cart = Cart(
+        product=add_product
+    )
+    db.session.add(new_cart)
+    db.session.commit()
+    return redirect('/')
 
 
 @app.route('/', methods=['GET'])
