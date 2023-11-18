5bec6f9f3d5a4ba4e50f196689bb1c0938860124
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-18 19:27:05 -0300

    Feat: Improved products page to receive product data

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ db.init_app(app)
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(255), unique=True, nullable=False)
     price = db.Column(db.Float, unique=True, nullable=False)
     description = db.Column(db.String(255), unique=True, nullable=False)
@@ -22,12 +23,29 @@ with app.app_context():
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    products = Product.query.all()
+    return render_template('index.html', products=products)
 
 
-@app.route('/product', methods=['GET'])
-def product():
-    return render_template('product.html')
+@app.route('/product/<int:id>', methods=['GET'])
+def product(id):
+    current_product = db.get_or_404(Product, id)
+    return render_template('product.html', product=current_product)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        new_product = Product(
+            name=request.form.get('prod_name'),
+            description=request.form.get('prod_desc'),
+            price=request.form.get('prod_price'),
+            image_url=request.form.get('prod_image')
+        )
+        db.session.add(new_product)
+        db.session.commit()
+        print('ok')
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
