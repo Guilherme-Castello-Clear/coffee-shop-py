12c62ffd589e7c73b0291a32b705657c51c6aa95
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-20 17:31:24 -0300

    Feat: Integrated stripe

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-import sqlite3
+from flask import Flask, render_template, request, redirect
 from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
+import stripe
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///shop.db"
 
 db = SQLAlchemy()
 db.init_app(app)
+
+stripe_keys = {
+  'secret_key': 'sk_test_51OEdFdDA8J9AhIG9ho7a3LK8Cnk6o5TDs3mG1K8L60rwDvEvrDGpLaKPjSwtvu5WE7L4VrEOHdV6EsCTrQfQTIVG00fYwcgA5H',
+  'publishable_key': 'pk_test_51OEdFdDA8J9AhIG9pDHiDoC0QrOrX5zwt8tnFs2JGpTMR3y8Rg1UbuT8uFYx3XaqGr3JmMA2ZzfkIQrz7YBdBydg00UVGof8yQ'
+}
+stripe.api_key = stripe_keys['secret_key']
 
 
 class Product(db.Model):
@@ -42,16 +48,19 @@ def remove_from_cart(cart_item_id):
     db.session.commit()
     return redirect('/cart')
 
+
 @app.route('/cart')
 def cart():
     cart = Cart.query.all()
     products = []
+    total_price = 0
     for item in cart:
         item_json = item.to_dict()
         item_product = db.get_or_404(Product, item_json['product_id'])
+        total_price += item_product.price
         products.append(item_product)
-
-    return render_template('cart.html', products=products)
+    total_price *= 100
+    return render_template('cart.html', products=products, key=stripe_keys['publishable_key'], total=total_price)
 
 
 @app.route('/add_to_cart/<int:id>', methods=["GET"])
@@ -88,8 +97,30 @@ def add():
         )
         db.session.add(new_product)
         db.session.commit()
-        print('ok')
     return render_template('add.html')
+
+
+@app.route('/checkout', methods=['POST'])
+def checkout():
+    cart = Cart.query.all()
+    total_price = 0
+    for item in cart:
+        item_json = item.to_dict()
+        item_product = db.get_or_404(Product, item_json['product_id'])
+        total_price += int(item_product.price * 100)
+
+    customer = stripe.Customer.create(
+        email='sample@customer.com',
+        source=request.form['stripeToken']
+    )
+    stripe.Charge.create(
+        customer=customer.id,
+        amount=total_price,
+        currency='brl',
+        description='Cafe'
+    )
+
+    return redirect('/')
 
 
 if __name__ == "__main__":
